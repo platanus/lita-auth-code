bb6dbc8c4c560f191fc77a069fc7de9e740be018
Guillermo Moreno <gmq@guillermomoreno.cl>
2018-08-31 11:58:37 -0300

    feat(auth-code): generate random pin

--- lib/lita/handlers/auth_code.rb
+++ lib/lita/handlers/auth_code.rb
@@ -1,7 +1,11 @@
 module Lita
   module Handlers
     class AuthCode < Handler
-      # insert handler code here
+      route(/^authme$/i, command: true) do |response|
+        pin = rand(100000..999999).to_s
+        redis.set(response.user.id, pin)
+        response.reply(pin)
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/auth_code_spec.rb
+++ spec/lita/handlers/auth_code_spec.rb
@@ -1,4 +1,14 @@
 require "spec_helper"
 
 describe Lita::Handlers::AuthCode, lita_handler: true do
+  before :each do
+    srand(1)
+  end
+
+  it { is_expected.to route_command("authme") }
+
+  it "responds to user with code" do
+    send_command("authme")
+    expect(replies.last).to eq("228037")
+  end
 end
